045b55e54bfab8d3e7bae2aa287b1e9f1a5bac04
thomas <siadousthomas@gmail.com>
2018-07-10 09:46:57 +0200

    Reference in HistoryActivity.java the textView and imageButton

--- app/src/main/java/com/thomas/siadous/moodtracker/HistoryActivity.java
+++ app/src/main/java/com/thomas/siadous/moodtracker/HistoryActivity.java
@@ -20,8 +20,26 @@ public class HistoryActivity extends AppCompatActivity {
      */
     private SharedPreferences mPreferencesHistory;
     private String mComment;
-    TextView mDisplayComment;
-    ImageButton mToastComment;
+
+    TextView mSevenDaysAgoTextView;
+    TextView mSixDaysAgoTextView;
+    TextView mFiveDaysAgoTextView;
+    TextView mFourDaysAgoTextView;
+    TextView mThreeDaysAgoTextView;
+    TextView mTwoDaysAgoTextView;
+    TextView mOneDayAgoTextView;
+
+
+    ImageButton mSevenDaysAgoComment;
+    ImageButton mSixDaysAgoComment;
+    ImageButton mFiveDaysAgoComment;
+    ImageButton mFourDaysAgoComment;
+    ImageButton mThreeDaysAgoComment;
+    ImageButton mTwoDaysAgoComment;
+    ImageButton mOneDayAgoComment;
+
+
+
     int mColorTest = R.color.banana_yellow;
 
     @Override
@@ -29,11 +47,29 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        mDisplayComment = findViewById(R.id.comment_test_preferences);
-        mToastComment = findViewById(R.id.comment_toast_imageButton_history);
+        // Reference the textView
+        mSevenDaysAgoTextView = findViewById(R.id.happy_block_textView_history);
+        mSixDaysAgoTextView = findViewById(R.id.super_happy_block_textView_history);
+        mFiveDaysAgoTextView = findViewById(R.id.normal_block_textView_history);
+        mFourDaysAgoTextView = findViewById(R.id.disappointed_block_textView_history);
+        mThreeDaysAgoTextView = findViewById(R.id.sad_block_textView_history);
+        mTwoDaysAgoTextView = findViewById(R.id.disappointed_2_block_textView_history);
+        mOneDayAgoTextView = findViewById(R.id.super_happy_2_block_textView_history);
+
+        // Reference the imageButton
+        mSevenDaysAgoComment = findViewById(R.id.comment_happy_imageButton_history);
+        mSixDaysAgoComment = findViewById(R.id.comment_super_happy_imageButton_history);
+        mFiveDaysAgoComment = findViewById(R.id.comment_normal_imageButton_history);
+        mFourDaysAgoComment = findViewById(R.id.comment_disappointed_imageButton_history);
+        mThreeDaysAgoComment = findViewById(R.id.comment_sad_imageButton_history);
+        mTwoDaysAgoComment = findViewById(R.id.comment_disappointed_2_imageButton_history);
+        mOneDayAgoComment = findViewById(R.id.comment_super_happy_2_imageButton_history);
+
+
+
         Log.d("DEBUG", "Enter onCreate");
 
-        mToastComment.setOnClickListener(new View.OnClickListener() {
+        mSevenDaysAgoComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("DEBUG", "Enter onClick");
@@ -44,16 +80,11 @@ public class HistoryActivity extends AppCompatActivity {
             }
         });
 
-        mDisplayComment.setText("Hier");
+        mSevenDaysAgoTextView.setText("Hier");
 
         //mDisplayComment.setBackgroundResource(R.color.banana_yellow);
 
-        /* RelativeLayout layout = findViewById(R.id.relativeLayout);
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) layout.getLayoutParams();
-        layoutParams.height = dpToPx(1280);
-        layoutParams.width = dpToPx(768);
-        layout.setLayoutParams(layoutParams);
-        */
+
 
     }
 
